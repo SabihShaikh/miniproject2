95409894df083c01aa1cb569c568923c64a429f5
SabihShaikh <90505713+SabihShaikh@users.noreply.github.com>
2021-09-24 21:44:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//selectors
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+window.addEventListener('scroll', function(){
+    let windowposition = window.scrollY> 0;
+    header.classList.toggle('active', window.scrollY> 0)
+})
+
+hamburgerMenu.addEventListener('click', function () {
+    header.classList.toggle('menu-open');
+})
